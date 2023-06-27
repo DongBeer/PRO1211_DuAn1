ce3304a5b680d7605ade180b5a86703ca18c4ccc
dongnvph <dongnguyen2802nb@gmail.com>
2023-06-27 23:20:05 +0700

    create project

---
+++ app/src/main/java/dongnvph30597/fpoly/app_labtopstore/MainActivity.java
@@ -0,0 +1,14 @@
+package dongnvph30597.fpoly.app_labtopstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
