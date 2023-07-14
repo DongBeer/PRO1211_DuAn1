4c681801a9e940c3a5fd22e7b88edbed0251b83a
luanntph17288 <luanntph17288@fpt.edu.vn>
2023-07-14 17:47:55 +0700

    fragment quan ly loai sp

---
+++ app/src/main/java/dongnvph30597/fpoly/app_labtopstore/Fragments/Fragment_QuanLyLoaiSp.java
@@ -0,0 +1,186 @@
+package dongnvph30597.fpoly.app_labtopstore.Fragments;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dongnvph30597.fpoly.app_labtopstore.R;
+import dongnvph30597.fpoly.app_labtopstore.adapter.LoaiSanPhamAdapter;
+import dongnvph30597.fpoly.app_labtopstore.dao.ThuongHieuDao;
+import dongnvph30597.fpoly.app_labtopstore.model.ThuongHieu;
+
+
+public class Fragment_QuanLyLoaiSp extends Fragment {
+
+    private FloatingActionButton fabAdd;
+    private Toolbar toolbar;
+    private RecyclerView rcv;
+    private List<ThuongHieu> list;
+    private LoaiSanPhamAdapter adapter;
+
+    private ThuongHieuDao dao;
+    ImageView img;
+    private String imagePath;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private static final int PERMISSION_REQUEST_CODE = 2;
+
+    public Fragment_QuanLyLoaiSp() {
+        // Required empty public constructor
+    }
+    public static Fragment_QuanLyLoaiSp newInstance() {
+        Fragment_QuanLyLoaiSp fragment = new Fragment_QuanLyLoaiSp();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quan_ly_loai_sp, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fabAdd = view.findViewById(R.id.fab_add);
+        rcv = view.findViewById(R.id.rcv_loaisp);
+        dao = new ThuongHieuDao(getContext());
+        list = new ArrayList<>();
+        list = dao.selectAll();
+        adapter= new LoaiSanPhamAdapter(getContext());
+        adapter.setData(list);
+        rcv.setAdapter(adapter);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getContext());
+                dialog.setContentView(R.layout.layout_add_loaisp);
+                Window window = dialog.getWindow();
+                window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                EditText edtName = dialog.findViewById(R.id.edit_name);
+                img = dialog.findViewById(R.id.btn_add_image);
+                CardView btnAdd = dialog.findViewById(R.id.btn_add);
+
+                ThuongHieu obj = new ThuongHieu();
+                img.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                            // Nếu quyền chưa được cấp, yêu cầu quyền
+                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+                        } else {
+                            // Nếu quyền đã được cấp, mở thư viện ảnh
+                            openImagePicker();
+                        }
+                    }
+                });
+                btnAdd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String name = edtName.getText().toString();
+                        if (!name.isEmpty() && imagePath != null) {
+                            obj.setTenTH(name);
+                            obj.setImgTH(imagePath);
+
+                            dao.insert(obj);
+                            list = dao.selectAll();
+                            adapter.setData(list);
+                            dialog.dismiss();
+                        } else {
+                            // Handle case when either name or imagePath is empty
+                            Toast.makeText(getContext(), "Please enter name and select an image", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+                dialog.show();
+            }
+        });
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            imagePath = getRealPathFromUri(selectedImage);
+            Glide.with(getContext())
+                    .load(selectedImage)
+                    .into(img);
+        }
+    }
+
+    private String getRealPathFromUri(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getActivity().getContentResolver().query(uri, projection, null, null, null);
+        int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String filePath = cursor.getString(columnIndex);
+        cursor.close();
+        return filePath;
+    }
+
+    private void openImagePicker() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Quyền đã được cấp, mở thư viện ảnh
+                openImagePicker();
+            } else {
+                // Quyền bị từ chối, hiển thị thông báo hoặc thực hiện xử lý khác
+                Toast.makeText(getContext(), "Permission denied. Unable to access external storage.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
--- app/src/main/java/dongnvph30597/fpoly/app_labtopstore/MainActivity.java
+++ app/src/main/java/dongnvph30597/fpoly/app_labtopstore/MainActivity.java
@@ -1,23 +1,31 @@
 package dongnvph30597.fpoly.app_labtopstore;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
+import dongnvph30597.fpoly.app_labtopstore.Fragments.Fragment_QuanLyLoaiSp;
 import dongnvph30597.fpoly.app_labtopstore.Fragments.Fragment_QuanlyKhachHang;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     public static DrawerLayout drawerLayout;
     private NavigationView navigationView;
 
     private TextView tvuser,tvuserma;
     private View view;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +33,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_layout);
+        toolbar = findViewById(R.id.id_toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
         getSupportFragmentManager().beginTransaction().replace(R.id.container_main,new Fragment_QuanlyKhachHang()).commit();
         navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.ic_client){
+            replaceFragment(new Fragment_QuanlyKhachHang());
+        }else if (id == R.id.ic_type_product){
+            replaceFragment(new Fragment_QuanLyLoaiSp());
+        }
+        drawerLayout.closeDrawer(navigationView);
+        return false;
+    }
+
+    // thay thế framelayout bằng fragment tương ứng khi chọn chức năng
+    public void replaceFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container_main, fragment);
+        transaction.commit();
     }
 }
